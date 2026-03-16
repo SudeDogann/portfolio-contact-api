cc6acd53d977f0efa2b567a00c208f734ca33f3f
Sude <sude@omnitive.com>
2026-03-16 19:16:46 +0300

    add frontend domain

--- PortfolioAPI/Program.cs
+++ PortfolioAPI/Program.cs
@@ -29,7 +29,7 @@ builder.Services.AddCors(options =>
     {
         policy.WithOrigins(
             "http://localhost:5173",
-            "https://yourfrontenddomain.com"
+            "https://portfolio-frontend-three-beige.vercel.app/"
         )
         .AllowAnyHeader()
         .AllowAnyMethod();
