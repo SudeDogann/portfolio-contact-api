7636a2de2d1ecc91393e0b3047e09ddba7c636a2
Sude <sude@omnitive.com>
2026-03-16 19:25:16 +0300

    update frontend domain

--- PortfolioAPI/Program.cs
+++ PortfolioAPI/Program.cs
@@ -29,7 +29,7 @@ builder.Services.AddCors(options =>
     {
         policy.WithOrigins(
             "http://localhost:5173",
-            "https://portfolio-frontend-three-beige.vercel.app/"
+            "https://portfolio-frontend-three-beige.vercel.app"
         )
         .AllowAnyHeader()
         .AllowAnyMethod();
